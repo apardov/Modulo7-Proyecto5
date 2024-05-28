48aa4de6a1d358c44228e416dc04c89741f4c355
Andres Pardo <and.pardo.vergara@gmail.com>
2024-05-27 20:15:06 -0400

    Proyecto 5 modulo 7

---
+++ gestion/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import register, EmailLoginView, EmailLogoutView, authenticated, home, order_details, order_status, \
+    take_order
+
+app_name = 'gestion'
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('login/', EmailLoginView.as_view(), name='login'),
+    path('logout/', EmailLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+    path('authenticated/', authenticated, name='authenticated'),
+    path('detail/<int:pedido_id>', order_details, name='detail'),
+    path('status/<int:pedido_id>', order_status, name='status'),
+    path('order/', take_order, name='order')
+]
